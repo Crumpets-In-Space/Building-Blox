80cabe0b11e5adb217e08638cd64da57a0e1872b
Bradley Jones <jones.bradley@me.com>
2013-03-09 00:56:46 -0800

    Actual physics and asteroid pic

--- entities/Asteroid.lua
+++ entities/Asteroid.lua
@@ -2,24 +2,28 @@ Asteroid = {}
 
 function Asteroid:new()
   local object = {
-    image = love.graphics.newImage("hamster.png"),
+    image = love.graphics.newImage("asteroid.png"),
     x = 150,
     y = 150,
     xSpeed = 0,
     ySpeed = 0
   }
 
+  -- Physics
+  object.body = love.physics.newBody(world, object.x,object.y, "dynamic")
+  object.shape = love.physics.newCircleShape(50)
+  object.fixture = love.physics.newFixture(object.body, object.shape):setUserData("Asteroid")-- connect body to shape
+  
   setmetatable(object, { __index = Asteroid })
   return object
 end
 
 -- Update function
 function Asteroid:update(dt)  
-  -- update the player's position
-  self.x = self.x + (self.xSpeed * dt)
-  self.y = self.y + (self.ySpeed * dt)
+  
 end
 
 function Asteroid:draw()
-  g.draw(self.image, self.x, self.y)
+  g.circle("line", self.body:getX(),self.body:getY(), self.shape:getRadius(), 20)
+  g.draw(self.image, self.body:getX(), self.body:getY(), self.body:getAngle(),  .07, .07, self.image:getWidth()/2, self.image:getHeight()/2)
 end
--- main.lua
+++ main.lua
@@ -1,6 +1,11 @@
 function love.load()
   -- graphics love
   g = love.graphics
+   text       = ""
+  -- Game world
+  world = love.physics.newWorld( 0, 0, true )
+    --These callback function names can be almost any you want:
+    world:setCallbacks(beginContact, endContact)
   
   -- instantiate our player and set initial values
   require "Player"
@@ -11,6 +16,9 @@ function love.load()
 end
 
 function love.update(dt)
+  --update World
+  world:update(dt)
+  
   -- update the player's position
   p:update(dt)
  
@@ -26,4 +34,16 @@ function love.draw()
    
    -- Draw asteroid
    a:draw()
+   
+   love.graphics.print(text, 10, 10)
+end
+
+function beginContact(a, b, coll)
+    x,y = coll:getNormal()
+    text = text.."\n"..a:getUserData().." colliding with "..b:getUserData().." with a vector normal of: "..x..", "..y
+end
+
+
+function endContact(a, b, coll)
+    text = text.."\n"..a:getUserData().." uncolliding with "..b:getUserData()
 end
